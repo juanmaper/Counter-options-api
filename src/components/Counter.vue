cce99feca19829a11ce388b49f173631ce5323bd
Juanma <juanmaper30@gmail.com>
2022-05-07 13:44:06 +0200

    Initial setup of the counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,68 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p> {{ counter }} <sup>2</sup> = {{ squaredValue }}</p>
+
+  <div>
+    <button @click="increase">+1</button>
+    <button @click="decrease">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 100,
+      validator(value) {
+        return value >= 0
+      }
+      //required: true,
+    }
+  },
+  data() {
+    return {
+      counter: this.start
+    }
+  },
+  methods: {
+    getSquaredValue() {
+      return this.counter ** 2
+    },
+    increase() {
+      this.counter++
+    },
+    decrease() {
+      this.counter--
+    }
+  },
+  computed: {
+    squaredValue() {
+      return this.counter ** 2
+    },
+    customTitle() {
+      return this.title || 'Counter'
+    }
+  }
+}
+</script>
+
+<style>
+
+button{
+    background-color: #64B687;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+}
+
+button:hover {
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+}
+</style>
