ab282023a2cbe755752fdb7ffe44838bd051a4a7
Juanma <juanmaper30@gmail.com>
2022-05-07 17:52:57 +0200

    Add http request to api and show answer with image

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -1,5 +1,5 @@
 <template>
-  <img src="https://via.placeholder.com/250" alt="bg">
+  <img v-if="img" :src="img" alt="bg">
   <div class="bg-dark"></div>
 
   <div class="indecision-container">
@@ -12,7 +12,7 @@
 
     <div>
       <h2> {{ question }} </h2>
-      <h1>Sí, No, ...pensando</h1>
+      <h1> {{ answer }} </h1>
 
     </div>
 
@@ -24,14 +24,27 @@
 export default {
   data() {
     return {
-      question: null
+      question: null,
+      answer: null,
+      img: null
     }
   },
   watch: {
     question(value, oldValue) {
       if ( !value.includes("?") ) return
 
+      this.getAnswer()
+    }
+  },
+  methods: {
+    async getAnswer() {
+      
+      this.answer = "Pensando..."
+      
+      const { answer, image } = await fetch("https://yesno.wtf/api").then( r => r.json() )
 
+      this.answer = answer
+      this.img = image
     }
   }
 }
