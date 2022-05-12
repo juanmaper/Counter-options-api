a2ee774467854a5567c4be4575025e9f55b77747
Juanma <juanmaper30@gmail.com>
2022-05-12 14:03:47 +0200

    Add test with spy

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -35,6 +35,8 @@ export default {
     question(value, oldValue) {
       this.isValidQuestion = false
 
+      console.log( value );
+
       if ( !value.includes("?") ) return
 
       this.isValidQuestion = true
--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -5,12 +5,34 @@ import Indecision from '@/components/Indecision'
 describe( 'Indecision Component', () => {
 
   let wrapper
+  let clgSpy
 
   beforeEach( () => {
     wrapper = shallowMount( Indecision )
+    clgSpy = jest.spyOn(console, 'log')
   })
 
   test('should match snapshot', () => {
     expect( wrapper.html() ).toMatchSnapshot()
+  })
+
+  test('should not trigger anything when you write input, except console.log', async() => {
+
+    const input = wrapper.find('input')
+    await input.setValue('Hola Mundo')
+
+    expect( clgSpy ).toHaveBeenCalledTimes(1)
+  })
+
+  test('should trigger the fetch when a "?" is written', () => {
+    
+  })
+
+  test('getAnswer test', () => {
+    
+  })
+
+  test('getAnswer test - API error', () => {
+    
   })
 })
